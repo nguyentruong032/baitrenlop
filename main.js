fead92d37e59ff7de2dfa0260333760f1675726f
nguyentruong032 <thesucceeder032045@gmail.com>
2024-02-10 01:02:50 +0700

    add

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
 function goback() {
   window.history.back();
 }
-document.getElementById("backToTopBtn").addEventListener("click", function () {
-  // Sử dụng window.scrollTo để cuộn về đầu trang web
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth", // Tạo hiệu ứng cuộn mượt nếu trình duyệt hỗ trợ
-  });
-});
